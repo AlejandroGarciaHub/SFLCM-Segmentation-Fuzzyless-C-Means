cfd20fafe0bc87635cdf048844c86d1a0ab5e5fe
Miguel Garcia <miguel_itecit@hotmail.com>
2018-12-03 11:26:37 -0600

    Initial Commit

---
+++ SFLCM - Segmentation Fuzzyless C-Means/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SFLCM - Segmentation Fuzzyless C-Means
+//
+//  Created by Miguel Garcia on 03/12/18.
+//  Copyright © 2018 Miguel Garcia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
